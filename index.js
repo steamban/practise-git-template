9a029f0c839cd571e8827d3b7ade463a364af93c
Arvind <nairarvind2011@gmail.com>
2023-01-28 00:05:08 +0530

    added ejs view engine to main index.js file

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 8000;
 // use express router middleware
 app.use("/", require("./routes"));
 
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error: ${err}`);
