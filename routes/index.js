3d98aa98df43c3ff3d9e6bc8f38038a3bcf1d28c
Arvind <nairarvind2011@gmail.com>
2023-01-27 23:52:58 +0530

    added users.js to routes folder and made users_controller.js file

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ const homeController = require('../controllers/home_controller');
 // for testing purposes
 console.log('router loaded');
 
+// for homepage
+// router.use('/routername', require('./routerfilename'));
 router.get('/', homeController.home);
+
+// for users (further mapping done in controllers folder with dedicated file)
+router.use('/users', require('./users'))
 
 module.exports = router;
